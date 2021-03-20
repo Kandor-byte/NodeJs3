ec705b7bd55bcd5fe358190e8a4e3af66c126f40
ShuxHop <shuxhop@pop-os.localdomain>
2021-03-20 18:10:08 +0200

    Learning Node.js

---
+++ app.js
@@ -0,0 +1,24 @@
+console.log("hey ninjas");
+
+var time = 0;
+
+//setTimeout(function(){
+//    console.log("3 seconds have passed");
+//}, 3000);
+
+
+//setInterval(function(){
+//    time += 2;
+//    console.log(time + " seconds passed");
+//}, 2000);
+
+//var timer = setInterval(function(){
+//    time++;
+//    console.log(time + " seconds have passed");
+//    if(time == 5){
+//        clearInterval(timer);
+//    }
+//}, 1000);
+
+console.log(__dirname);
+console.log(__filename);
